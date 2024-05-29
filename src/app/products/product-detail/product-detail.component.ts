4787e26d0baf9e90e17ca53dde4c99c0b0979595
Cesar Mendoza <52425885+mendoce24@users.noreply.github.com>
2024-05-29 15:16:24 +0200

    implementing Observable of product

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,50 +1,35 @@
-import { Component, Input, OnChanges, OnDestroy, SimpleChanges, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 
-import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
+import { NgIf, NgFor, CurrencyPipe, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
-import { EMPTY, Subscription, catchError, tap } from 'rxjs';
+import { EMPTY, catchError, tap } from 'rxjs';
 import { ProductService } from '../product.service';
 
 @Component({
     selector: 'pm-product-detail',
     templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  standalone: true,
+  imports: [AsyncPipe, NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent implements OnChanges, OnDestroy{
-  @Input() productId: number = 0;
+export class ProductDetailComponent {
   errorMessage = '';
-  sub!: Subscription;
 
   // Product to display
   private productService = inject(ProductService);
-  product: Product | null = null;
+  product$ = this.productService.product$
+    .pipe(
+      tap(() => console.log('Pipeline Product retrieved')),
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
 
   // Set the page title
-  pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  //pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  pageTitle = '';
 
   addToCart(product: Product) {
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    const id = changes['productId'].currentValue;
-    if (id) {
-      this.sub = this.productService.getProduct(id)
-        .pipe(
-          tap(() => console.log('Pipeline Product retrieved')),
-          catchError(err => {
-            this.errorMessage = err;
-            return EMPTY;
-          })
-        ).subscribe({
-          next: product => this.product = product
-        });
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.sub) {
-      this.sub.unsubscribe();
-    }
-  }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, catchError, map, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, filter, map, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
@@ -19,6 +19,9 @@ export class ProductService {
   private errorService = inject(HttpErrorService);
   private reviewService = inject(ReviewService);
 
+  private productSelectedSubject = new BehaviorSubject<number | undefined>(undefined);
+  readonly productSelected$ = this.productSelectedSubject.asObservable();
+
   readonly products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
       tap(p => console.log(JSON.stringify(p))),
@@ -26,14 +29,22 @@ export class ProductService {
       catchError(err => this.handleError(err))
     );
 
-  getProduct(id: number): Observable<Product> {
-    const productUrl = `${this.productsUrl}/${id}`;
-    return this.http.get<Product>(productUrl)
-      .pipe(
-        tap(() => console.log('In http.get by id pipeline')),
-        switchMap(product => this.getProductWithReviews(product)),
-        catchError(err => this.handleError(err))
-      );
+  readonly product$ = this.productSelected$
+    .pipe(
+      filter(Boolean),
+      switchMap(id => {
+        const productUrl = `${this.productsUrl}/${id}`;
+        return this.http.get<Product>(productUrl)
+          .pipe(
+            tap(() => console.log('In http.get by id pipeline')),
+            switchMap(product => this.getProductWithReviews(product)),
+            catchError(err => this.handleError(err))
+          );
+      })
+    );
+
+  productSelected(selectedProductId: number): void {
+    this.productSelectedSubject.next(selectedProductId);
   }
 
   private getProductWithReviews(product: Product): Observable<Product> {
